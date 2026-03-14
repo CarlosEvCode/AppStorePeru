0d3296c617361748a91bad51db45e9ed72ee0662
CarlosEvCode <programer.cm12@gmail.com>
2026-03-14 09:00:22 -0500

    Creando los activities Listar y Registrar, y diseñando el layout del main con la navegacion basica

--- app/src/main/java/com/example/appstoreperu/Registrar.java
+++ app/src/main/java/com/example/appstoreperu/Registrar.java
@@ -1,8 +1,6 @@
 package com.example.appstoreperu;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,34 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-
-    Button btnActivityListar, btnActivityRegistrar;
-
-    private void loadUI() {
-        btnActivityListar = findViewById(R.id.btnActivityListar);
-        btnActivityRegistrar = findViewById(R.id.btnActivityRegistrar);
-    }
+public class Registrar extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_registrar);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-
-        loadUI();
-
-        btnActivityRegistrar.setOnClickListener((v) -> {
-            startActivity(new Intent(getApplicationContext(), Registrar.class));
-        });
-
-        btnActivityListar.setOnClickListener((v) -> {
-            startActivity(new Intent(getApplicationContext(), Listar.class));
         });
     }
 }
